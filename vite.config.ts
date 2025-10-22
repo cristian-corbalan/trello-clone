90dd8b78353ae4d46d3cab75e86ac9c381e30d74
cristian-corbalan <ccorbalandw@gmail.com>
2025-10-22 18:12:51 -0300

    Create an alias to the src folder on tsconfig and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import * as path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
 });
